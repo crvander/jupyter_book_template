23b501709f5823fca783c3b29384e725ac3de5ed
Carlos Ricco van der Ley <63980759+crvander@users.noreply.github.com>
2024-10-21 12:36:02 -0700

    test

---
+++ docs/conf.py
@@ -0,0 +1,19 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'Project Jupyter Book Template'
+copyright = '2024, Carlos van der Ley'
+author = 'Carlos van der Ley'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
